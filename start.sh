df32f0392fb8295dd42455ce8634421750a85fe0
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-04-05 17:49:14 +0100

    Do not error if no sessions are running

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill >/dev/null 2>&1 || echo
 make mprestart
 screen -d -m make polling
 screen -d -m make server

8fad7d430aa99225e3d19af0976b468fd2735714
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-04-05 16:49:09 +0100

    Ensure all sessions are closed

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
 sudo systemctl start nginx
 screen -d -m make polling
 screen -d -m sudo make server

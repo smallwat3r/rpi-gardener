0545ea81de8e415246499ffaa6f9c3a0e32a3e83
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-04-05 17:00:03 +0100

    Remove script

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
-sudo systemctl start nginx
-screen -d -m make polling
-screen -d -m sudo make server

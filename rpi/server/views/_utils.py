39b4d87d70d8f026283dafec714256dc7c4f4060
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-04-06 16:15:07 +0100

    Default to 3 hours

--- rpi/server/views/_utils.py
+++ rpi/server/views/_utils.py
@@ -3,13 +3,12 @@ from datetime import datetime, timedelta
 from flask import Request
 
 
-class BadParameter(Exception):
-    ...
+class BadParameter(Exception): ...
 
 
 def get_qs(request: Request) -> tuple[int, datetime]:
     try:
-        hours = int(request.args.get("hours", 1))
+        hours = int(request.args.get("hours", 3))
     except ValueError as err:
         raise BadParameter("Parameter needs to be an integer") from err
     if hours > 24:

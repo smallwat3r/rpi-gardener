a34c0fbe045ffcc93a5986847a406f1a74c6682c
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-10-20 16:36:18 +0100

    Revert grep

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-screen -ls | cut -d. -f1 | awk '{print $1}' | xargs kill >/dev/null 2>&1 || echo
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill >/dev/null 2>&1 || echo
 make mprestart
 screen -dS polling -m make polling
 screen -dS server -m make server

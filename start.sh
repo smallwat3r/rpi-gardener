04dc95bd43b5029a6e1567919cd3c96ec7a3b7b9
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-10-20 16:29:24 +0100

    Small updates

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill >/dev/null 2>&1 || echo
+screen -ls | cut -d. -f1 | awk '{print $1}' | xargs kill >/dev/null 2>&1 || echo
 make mprestart
-screen -d -m make polling
-screen -d -m make server
+screen -dS polling -m make polling
+screen -dS server -m make server
 sudo systemctl start nginx

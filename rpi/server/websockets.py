d03879bfa12a2cac61f6716a66ebd5de90662ee4
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-12-23 18:32:51 +0100

    Refactor

--- rpi/server/websockets.py
+++ rpi/server/websockets.py
@@ -1,6 +1,7 @@
 """WebSocket routes for the RPi Gardener application."""
 import asyncio
 from datetime import datetime, timedelta
+from typing import Any, Callable
 
 from starlette.websockets import WebSocket, WebSocketDisconnect
 
@@ -12,13 +13,35 @@ from rpi.lib.db import (
     get_stats_dht_data,
 )
 
-logger = logging.getLogger("websockets")
-
+_logger = logging.getLogger("websockets")
 _DEFAULT_HOURS = 3
 
 
-def _get_from_time(websocket: WebSocket) -> datetime:
-    """Parse hours from query string and return the from_time datetime."""
+async def _stream_data(
+    websocket: WebSocket,
+    fetch_fn: Callable[[], Any],
+    endpoint: str,
+) -> None:
+    """Stream data to a WebSocket client at regular intervals."""
+    await websocket.accept()
+    client_id = id(websocket)
+    _logger.info("Client %s connected to %s", client_id, endpoint)
+
+    try:
+        while True:
+            await asyncio.sleep(POLLING_FREQUENCY_SEC)
+            try:
+                await websocket.send_json(fetch_fn())
+            except WebSocketDisconnect:
+                raise
+            except Exception as e:
+                _logger.error("Error streaming to client %s: %s", client_id, e)
+    except WebSocketDisconnect:
+        _logger.info("Client %s disconnected", client_id)
+
+
+def _parse_hours(websocket: WebSocket) -> datetime:
+    """Parse hours query param and return from_time datetime."""
     try:
         hours = int(websocket.query_params.get("hours", _DEFAULT_HOURS))
     except ValueError:
@@ -28,54 +51,19 @@ def _get_from_time(websocket: WebSocket) -> datetime:
 
 async def ws_dht_latest(websocket: WebSocket) -> None:
     """Stream latest DHT sensor readings."""
-    await websocket.accept()
-    client_id = id(websocket)
-    logger.info("WebSocket client %s connected to /dht/latest", client_id)
-
-    try:
-        while True:
-            await asyncio.sleep(POLLING_FREQUENCY_SEC)
-            try:
-                data = get_latest_dht_data()
-                await websocket.send_json(data)
-            except Exception as e:
-                logger.error("Error fetching DHT data for client %s: %s", client_id, e)
-    except WebSocketDisconnect:
-        logger.info("WebSocket client %s disconnected", client_id)
+    await _stream_data(websocket, get_latest_dht_data, "/dht/latest")
 
 
 async def ws_dht_stats(websocket: WebSocket) -> None:
     """Stream DHT sensor statistics."""
-    await websocket.accept()
-    client_id = id(websocket)
-    from_time = _get_from_time(websocket)
-    logger.info("WebSocket client %s connected to /dht/stats", client_id)
-
-    try:
-        while True:
-            await asyncio.sleep(POLLING_FREQUENCY_SEC)
-            try:
-                data = get_stats_dht_data(from_time)
-                await websocket.send_json(data)
-            except Exception as e:
-                logger.error("Error fetching DHT stats for client %s: %s", client_id, e)
-    except WebSocketDisconnect:
-        logger.info("WebSocket client %s disconnected", client_id)
+    from_time = _parse_hours(websocket)
+    await _stream_data(
+        websocket,
+        lambda: get_stats_dht_data(from_time),
+        "/dht/stats",
+    )
 
 
 async def ws_pico_latest(websocket: WebSocket) -> None:
     """Stream latest Pico sensor readings."""
-    await websocket.accept()
-    client_id = id(websocket)
-    logger.info("WebSocket client %s connected to /pico/latest", client_id)
-
-    try:
-        while True:
-            await asyncio.sleep(POLLING_FREQUENCY_SEC)
-            try:
-                data = get_latest_pico_data()
-                await websocket.send_json(data)
-            except Exception as e:
-                logger.error("Error fetching Pico data for client %s: %s", client_id, e)
-    except WebSocketDisconnect:
-        logger.info("WebSocket client %s disconnected", client_id)
+    await _stream_data(websocket, get_latest_pico_data, "/pico/latest")

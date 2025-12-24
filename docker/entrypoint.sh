df72e7a22bf52634f2a036adae5964cc0ef92914
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-12-24 16:17:46 +0100

    Fix entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,16 @@ echo "Validating configuration..."
 python -c "from rpi.lib.config import validate_config; validate_config()"
 
 echo "Initializing database..."
-python -c "import asyncio; from rpi.lib.db import init_db; asyncio.run(init_db())"
+python -c "
+import asyncio
+from rpi.lib.db import init_db, close_async_db
+
+async def main():
+    await init_db()
+    await close_async_db()
+
+asyncio.run(main())
+"
 
 echo "Starting application..."
 exec "$@"

f8646dc70795974f3a78f7ff35c037b996f097eb
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2026-01-08 16:48:58 +0000

    Fix query

--- rpi/lib/sql/pico_chart.sql
+++ rpi/lib/sql/pico_chart.sql
@@ -1,6 +1,12 @@
-SELECT (recording_time / :bucket * :bucket) * 1000 as epoch
-     , json_group_object(CAST(plant_id AS TEXT), ROUND(AVG(moisture), 1)) as plants
-FROM pico_reading
-WHERE recording_time >= :from_epoch
-GROUP BY recording_time / :bucket
+SELECT epoch
+     , json_group_object(plant_id, moisture) as plants
+FROM (
+    SELECT (recording_time / :bucket * :bucket) * 1000 as epoch
+         , CAST(plant_id AS TEXT) as plant_id
+         , ROUND(AVG(moisture), 1) as moisture
+    FROM pico_reading
+    WHERE recording_time >= :from_epoch
+    GROUP BY recording_time / :bucket, plant_id
+)
+GROUP BY epoch
 ORDER BY epoch DESC

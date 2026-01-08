2cd72c2951681d215dae363b9320cff1e4a02f60
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2026-01-08 16:23:47 +0000

    Better query

--- rpi/lib/sql/pico_latest_recording.sql
+++ rpi/lib/sql/pico_latest_recording.sql
@@ -1,13 +1,10 @@
-SELECT plant_id
-     , moisture
-     , epoch
-     , recording_time
-FROM (
-    SELECT plant_id
-         , moisture
-         , recording_time * 1000 as 'epoch'
-         , datetime(recording_time, 'unixepoch') as 'recording_time'
-         , ROW_NUMBER() OVER (PARTITION BY plant_id ORDER BY recording_time DESC) as rn
+SELECT p.plant_id
+     , p.moisture
+     , p.recording_time * 1000 as 'epoch'
+     , datetime(p.recording_time, 'unixepoch') as 'recording_time'
+FROM pico_reading p
+INNER JOIN (
+    SELECT plant_id, MAX(recording_time) as max_time
     FROM pico_reading
-)
-WHERE rn = 1
+    GROUP BY plant_id
+) latest ON p.plant_id = latest.plant_id AND p.recording_time = latest.max_time
